20e5fd9d043540c9c2cf72b6413d87dd87b217a3
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-08-10 11:58:30 +0530

    Add Articles by API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Navbar, NewsCard } from "./components";
 
 function App() {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=4fed5d128d4a41279852beb42efa1063`);
+
+        if (!res.ok) throw new Error("Failed to fetch articles");
+        
+        const json = await res.json();
+
+        setArticles(json["articles"]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchArticles();
+  }, [])
+  
+
   return (
     <>
       <Navbar />
-      <div className="flex-grow-1 grid grid-cols-1 items-center md:grid-cols-2 xl:grid-cols-3 py-10 justify-items-center mx-auto gap-y-7 sm:gap-x-3 lg:gap-x-7">
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
+      <div className="flex-grow-1 grid grid-cols-1  md:grid-cols-2 xl:grid-cols-3 py-10 justify-items-center mx-auto gap-y-7 sm:gap-x-3 lg:gap-x-7">
+        {articles.length > 0 && articles.map((article, index) => (
+          <NewsCard key={index} {...article}/>
+        ))}
       </div>
     </>
   );
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function NewsCard() {
-  const dateTime = new Date("2025-08-09T02:02:59Z")
+function NewsCard({
+  source,
+  author,
+  urlToImage,
+  title,
+  description,
+  publishedAt,
+  content,
+  url,
+}) {
+  const dateTime = new Date(publishedAt)
     .toLocaleString("en-IN", {
       timeZone: "Asia/Kolkata",
       hour12: true,
@@ -9,45 +18,37 @@ function NewsCard() {
       month: "2-digit",
       year: "2-digit",
       hour: "numeric",
-      minute: "numeric"
+      minute: "numeric",
     })
     .replace(/\b(am|pm)\b/i, (m) => m.toUpperCase());
+
+  const modifiedContent = content?.split("[+")[0] || content;
 
   return (
     <div className="card bg-base-100 w-96 shadow-sm max-h-fit transition-transform duration-300 ease-in-out scale(100) hover:scale-105 hover:shadow-lg hover:dark:shadow-gray-900">
       <figure>
         <img
-          src="https://nbcsports.brightspotcdn.com/dims4/default/af7f598/2147483647/strip/true/crop/5446x3063+0+0/resize/1440x810!/quality/90/?url=https%3A%2F%2Fnbc-sports-production-nbc-sports.s3.us-east-1.amazonaws.com%2Fbrightspot%2Fb1%2F60%2F89c436814ee2b7bbd931a0f96856%2Fhttps-delivery-gettyimages.com%2Fdownloads%2F2229151887"
-          alt="Shoes"
+          src={urlToImage ?? "/Image_not_available.png"}
+          alt={source["name"]}
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          Browns rookie Shedeur Sanders throws two touchdown passes - NBC Sports
-        </h2>
-        <div className="dark:text-gray-300 text-gray-500 text-xs flex-between my-2">
-          <span>Author | Charean Williams</span>
-          <span>{dateTime}</span>
+        <h2 className="card-title">{title}</h2>
+        <div className="dark:text-gray-300 text-gray-500 text-xs flex justify-between my-2">
+          <div className="flex flex-col gap-1">
+            <span>Author | {author ?? "Anonymus"}</span>
+            <span>Source | {source["name"] ?? "Anonymus"}</span>
+          </div>
+          <span className="whitespace-nowrap">{dateTime} IST</span>
         </div>
         <p className="text-xs dark:text-gray-300 text-gray-500">
-          Browns fans will be calling for Shedeur Sanders to win the starting
-          job after his performance Friday night.
+          {description ?? "---"}
         </p>
-        <p>
-          Browns fans will be calling for Shedeur Sanders to win the starting
-          job after his performance Friday night.The rookie quarterback created
-          a buzz by completing 14 of 23 passes for 138 yards and two …
-          {/* [+702 chars] */}
-        </p>
+        <p>{modifiedContent ?? "---"}</p>
         <div className="card-actions justify-end">
           <button
             className="btn btn-primary"
-            onClick={() =>
-              open(
-                "https://www.nbcsports.com/nfl/profootballtalk/rumor-mill/news/browns-rookie-shedeur-sanders-throws-two-touchdown-passes",
-                "_blank"
-              )
-            }
+            onClick={() => open(url, "_blank")}
           >
             Read More
           </button>
